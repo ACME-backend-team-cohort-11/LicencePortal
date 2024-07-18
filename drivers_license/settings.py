35d378d20c19fef2c5d565fa9d8ad8942d176877
Richie Mugambi Ireri <79042120+RichieMuga@users.noreply.github.com>
2024-07-18 20:35:38 +0300

    fix: add allow hosts

--- drivers_license/settings.py
+++ drivers_license/settings.py
@@ -224,3 +224,5 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),  # Example refresh token lifetime
 }
+
+ALLOWED_HOSTS = ['*']
